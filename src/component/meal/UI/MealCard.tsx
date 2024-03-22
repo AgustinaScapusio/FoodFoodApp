dc5c00224a3133a606b6ef65ab4f62c2c676713d
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-03-22 16:12:39 +0100

    Refactor MealCard component to use Redux store

--- src/component/meal/UI/MealCard.tsx
+++ src/component/meal/UI/MealCard.tsx
@@ -1,21 +1,21 @@
-import { useNavigate } from "react-router-dom";
+import { useAppSelector,} from "../../../../store";
 import { Meal } from "../../../types/types";
 
 export function MealCard({meal}: {meal: Meal}) {
-    const navigate= useNavigate();
+  const { data: restaurantData } = useAppSelector((state) => state.restaurants);
 
-    const handlerClick = () => {
-        navigate(`/meals/${meal.id}`);
-    }
+  const restaurant = restaurantData.find((restaurant) => restaurant.id === meal.restaurantId);
 
     return (
-        <div onClick={handlerClick} className={
+        <div  className={
               "max-w-96 box shadow flex flex-col justify-between gap-2 cursor-pointer"
             }>
             <img src={meal.mealImage} alt={meal.name} className={"object-cover h-[240px]"}/>
             <div className={"p-2"}>
             <p className={"text-4xl font-extralight"}>{meal.name}</p>
+            {restaurant && <p className={"text-xl font-light"}>Restaurant: {restaurant.name}</p>}
             <p className={"text-xl font-light"}>{meal.category}</p>
+            <p className={"text-xl font-light"}>{meal.price}</p>
             </div>
         </div>
     );
