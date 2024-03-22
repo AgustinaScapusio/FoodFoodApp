21c008c8c433a69672b0d2bf63b5439d88dfb1c6
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-03-22 11:49:21 +0100

    Add RestaurantsPage to router

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,9 @@
 import { Meals } from "../component/Meals";
-import { Restaurants } from "../component/restaurant/Restaurants";
 
 export function HomePage() {
   return (
     <div className={"bg-yellow-600 text-blue-800"}>
       <Meals />
-      <Restaurants />
     </div>
   );
 }
