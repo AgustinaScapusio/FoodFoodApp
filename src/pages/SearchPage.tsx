824bcdfd1a02b8d250f4595dad06dfcc2e025797
Arne Ngo <arnengo2812@hotmail.com>
2024-03-22 13:38:54 +0100

    search :(

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,14 +1,8 @@
-import { RootState } from "../../store"
-import { useDispatch, useSelector } from "react-redux"
-import { decrement, increment } from "../../store/searchSlice";
+import { Search } from "../component/search/Searchs"
 
 export function SearchPage() {
-	const count = useSelector((state : RootState) => state.oogabooga.value)
-	const dispatch = useDispatch();
+
 	return <>
-	<h1>{count}</h1>
-	<input></input>
-	<button onClick={() => dispatch(increment())}>plus</button>
-	<button onClick={() => dispatch(decrement())}>minus</button>
+	<Search/>
 	</>
 }
