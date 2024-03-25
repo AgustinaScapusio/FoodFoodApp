dcc7b74e5b19b5ccac32ef2d06c6c7272cda2965
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-03-25 14:04:18 +0100

    Commented out unused code in NavBar component

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -2,28 +2,28 @@ import { Link } from "react-router-dom";
 import logo from "../assets/vectorstock_46110475_transparent.png";
 import Button from "./Button";
 import { useAppSelector } from "../store/index";
-import {Cart} from "./cart/Cart";
-
 import "./NavBar.css";
-import { useState } from "react";
+// import {Cart} from "./cart/Cart";
+
+// import { useState } from "react";
 export default function NavBar() {
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
   const { data } = useAppSelector((state) => state.cart);
   const total = data.reduce((acc: number, item: { quantity: number; }) => acc + item.quantity, 0);
-  const openModalHandler = () => {
-    const modal = document.querySelector(".modal");
-    modal?.classList.add("open");
-    setOpen(true);
-  };
+  // const openModalHandler = () => {
+  //   const modal = document.querySelector(".modal");
+  //   modal?.classList.add("open");
+  //   setOpen(true);
+  // };
 
-  const closeModalHandler = () => {
-    const modal = document.querySelector(".modal");
-    modal?.classList.remove("open");
-    setOpen(false);
-  }
+  // const closeModalHandler = () => {
+  //   const modal = document.querySelector(".modal");
+  //   modal?.classList.remove("open");
+  //   setOpen(false);
+  // }
   return (
     <nav className="navbar">
-    {open && <Cart closeModal={closeModalHandler} />}
+    {/* {open && <Cart closeModal={closeModalHandler} />} */}
       <div className="logo-container">
         <img src={logo} alt="logo" />
         <h1 className={"text-zinc-300"}>FoodFood</h1>
@@ -39,7 +39,7 @@ export default function NavBar() {
           <li>
             <Link to="/profile">Profile</Link>
           </li>
-          <Button className="cart" text={`Cart (${total})`} onClick={openModalHandler} />
+          <Button className="cart" text={`Cart (${total})`} onClick={() => {}} />
         </ul>
       </div>
     </nav>
