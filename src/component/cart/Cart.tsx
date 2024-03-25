ca0065d1116e57f9aac143541e28c9732c25500c
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-03-25 10:09:01 +0100

    Commented out unused code in Cart component

--- src/component/cart/Cart.tsx
+++ src/component/cart/Cart.tsx
@@ -1,49 +1,49 @@
-import { useAppDispatch, useAppSelector } from "../../../store";
-import { Order } from "../../types/types";
+// import { useAppDispatch, useAppSelector } from "../../../store";
+// import { Order } from "../../types/types";
 
-type CartProps = {
-  closeModal: () => void;
-};
+// type CartProps = {
+//   closeModal: () => void;
+// };
 
-export function Cart({ closeModal }: CartProps ){
-    const dispatch = useAppDispatch();
-    const cartItems = useAppSelector((state) => state.cart.data);
-  const meal = useAppSelector((state) => state.meals.data);
+// export function Cart({ closeModal }: CartProps ){
+//     const dispatch = useAppDispatch();
+//     const cartItems = useAppSelector((state) => state.cart.data);
+//   const meal = useAppSelector((state) => state.meals.data);
 
-    // const handleRemoveItem = (item: Order) => {
-    //     dispatch(removeQuantity(item));
-    // };
+//     // const handleRemoveItem = (item: Order) => {
+//     //     dispatch(removeQuantity(item));
+//     // };
 
-    // const handleAddItem = (item: Order) => {
-    //     dispatch(addQuantity(item));
-    // };
+//     // const handleAddItem = (item: Order) => {
+//     //     dispatch(addQuantity(item));
+//     // };
    
 
-  const mealData = meal.filter((meal) => cartItems.some((item) => item.id === meal.id))[0];
+//   const mealData = meal.filter((meal) => cartItems.some((item) => item.id === meal.id))[0];
 
 
 
-  return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={closeModal}>
-          &times;
-        </span>
-        <h2>Cart Items</h2>
-        <ul>
-          {cartItems.map((item) => (
-            <li key={item.id}>
-              <div>
-                <span>{mealData.name}</span>
-                <span>{mealData.price}</span>
-                <button onClick={() => handleAddItem(item)}>+</button>
-                <span>{item.quantity}</span>
-                <button onClick={() => handleRemoveItem(item)}>-</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="modal">
+//       <div className="modal-content">
+//         <span className="close" onClick={closeModal}>
+//           &times;
+//         </span>
+//         <h2>Cart Items</h2>
+//         <ul>
+//           {cartItems.map((item) => (
+//             <li key={item.id}>
+//               <div>
+//                 <span>{mealData.name}</span>
+//                 <span>{mealData.price}</span>
+//                 {/* <button onClick={() => handleAddItem(item)}>+</button> */}
+//                 <span>{item.quantity}</span>
+//                 {/* <button onClick={() => handleRemoveItem(item)}>-</button> */}
+//               </div>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
