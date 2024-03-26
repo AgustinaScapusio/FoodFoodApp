615438106810c90745b020620251cf03ed5b9fc2
brobo1 <armanahz95@gmail.com>
2024-03-26 16:34:12 +0100

    uno-reverse

--- src/component/profile/UI/ProfileCard.tsx
+++ src/component/profile/UI/ProfileCard.tsx
@@ -1,10 +1,14 @@
 import { UserType } from "../../../types/types";
 
-
-export function ProfileCard(profileData : UserType) {
-    return <>
-    <img className="" 
-    src="https://media.discordapp.net/attachments/1211580822985908234/1220724207344943165/IMG_4508.png?ex=660ffb02&is=65fd8602&hm=c77253dc9abaf0fe2a054086d3a2219797186bfa3e97da7de19f6c6b69d5da56&=&format=webp&quality=lossless&width=263&height=350"></img>
-    <h1>{profileData.userName}</h1>
+export function ProfileCard(profileData: UserType) {
+  return (
+    <>
+      <img
+        alt={"gottem"}
+        className=""
+        src="https://media.discordapp.net/attachments/1191814565021888693/1222206691991617687/image.png?ex=66155fae&is=6602eaae&hm=83f14afaed43ff1621a4fddd1c6825e08c24d393dcd430109b7752246e690613&=&format=webp&quality=lossless&width=491&height=509"
+      ></img>
+      <h1>{profileData.userName}</h1>
     </>
+  );
 }
