ddb5bec93b0206087ef58e46bd91d5a7c65fd537
Arne Ngo <arnengo2812@hotmail.com>
2024-03-26 15:38:51 +0100

    profile merge

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -3,6 +3,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { mealSlice } from "./mealSlice.tsx";
 import { restaurantSlice } from "./restaurantSlice.tsx";
 import { cartSlice } from "./cartSlice.tsx";
+import { userSlice } from "./userSlice.tsx";
+import { userProgressSlice } from "./userProgressSlice.tsx";
 
 export const store = configureStore({
   reducer: {
