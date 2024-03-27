b9b820634cdad59d59cc4eec6086ef7d96582961
Arne Ngo <arnengo2812@hotmail.com>
2024-03-27 11:34:23 +0100

    profile update

--- src/component/profile/UI/ProfileCard.tsx
+++ src/component/profile/UI/ProfileCard.tsx
@@ -1,14 +1,17 @@
 import { UserType } from "../../../types/types";
+import profilepic from "../../../assets/asdadsadsa.jpg"
 
 export function ProfileCard(profileData: UserType) {
   return (
     <>
+    <div>
       <img
-        alt={"gottem"}
-        className=""
-        src="https://media.discordapp.net/attachments/1191814565021888693/1222206691991617687/image.png?ex=66155fae&is=6602eaae&hm=83f14afaed43ff1621a4fddd1c6825e08c24d393dcd430109b7752246e690613&=&format=webp&quality=lossless&width=491&height=509"
-      ></img>
-      <h1>{profileData.userName}</h1>
+          className={"w-24 rounded-full shadow-lg"}
+          src={profilepic}
+          alt={"gottem"}
+          ></img>
+        <h1>{profileData.userName}</h1>
+        </div>
     </>
   );
 }
