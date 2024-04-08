e4c60c9eb5dce103a60f4c90280dff7b6c40fef2
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-04-08 10:04:31 +0200

    Add carousel to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Carousel } from "antd";
 import { Restaurants } from "../component/restaurant/Restaurants";
 
 export function HomePage() {
@@ -9,6 +10,36 @@ export function HomePage() {
       <p className={"text-2xl font-extralight text-center mt-5 mb-5"}>
         Find your favorite restaurant
       </p>
+      <Carousel autoplay>
+        <div>
+          <img
+            src="https://source.unsplash.com/random/800x400"
+            alt="restaurant"
+            className={"w-full h-96 object-cover"}
+          />
+        </div>
+        <div>
+          <img
+            src="https://source.unsplash.com/random/800x401"
+            alt="restaurant"
+            className={"w-full h-96 object-cover"}
+          />
+        </div>
+        <div>
+          <img
+            src="https://source.unsplash.com/random/800x402"
+            alt="restaurant"
+            className={"w-full h-96 object-cover"}
+          />
+        </div>
+        <div>
+          <img
+            src="https://source.unsplash.com/random/800x403"
+            alt="restaurant"
+            className={"w-full h-96 object-cover"}
+          />
+        </div>
+      </Carousel>
       <Restaurants />
     </>
   );
