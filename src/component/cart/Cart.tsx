343436ff64810eeade473e5a285f23515d30354b
brobo1 <armanahz95@gmail.com>
2024-04-11 11:40:35 +0200

    Update profile component and add token loader
    The update removes the unused import from "ProfileHttp". Unnecessary code was removed to make the component cleaner. In addition, a token loader was added to the root page in "App.tsx", retrieving the token from the local storage.

--- src/component/cart/Cart.tsx
+++ src/component/cart/Cart.tsx
@@ -1,6 +1,12 @@
 import { Modal } from "./CartModal.tsx";
 import { useAppDispatch, useAppSelector } from "../../store";
 import { closeModal } from "../../store/userProgressSlice.tsx";
+import { Button, InputNumber, Typography } from "antd";
+import {
+  CloseOutlined,
+  DeleteFilled,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
 
 export function Cart() {
   const cart = useAppSelector((state) => state.cart);
@@ -14,29 +20,60 @@ export function Cart() {
 
   return (
     <Modal className={"float-right bg-white w-96 h-dvh z-30"} open={modal}>
-      <h1 className={"text-xl font-bold text-center mb-4"}>Your Cart</h1>
-      <div>
-        {cart.data.map((item) => {
-          const mealItem = meal.find(
-            (meal) =>
-              meal.id === item.mealId &&
-              meal.restaurantId === item.restaurantId,
-          );
-          return (
-            <div key={item.mealId}>
-              <p>{mealItem?.name}</p>
-              <p>{item.quantity}</p>
-            </div>
-          );
-        })}
+      <div className={"flex flex-col justify-between h-full "}>
+        <div className={" "}>
+          <div className={"flex justify-between "}>
+            <h1 className={"text-3xl font-light p-6 pt-12"}>Your Cart</h1>
+            <Button
+              className={"m-2 w-full"}
+              shape={"circle"}
+              size={"large"}
+              icon={<CloseOutlined />}
+              onClick={handleClose}
+            />
+          </div>
+          <div className={"border-t "}>
+            {cart.data.map((item) => {
+              const mealItem = meal.find(
+                (meal) =>
+                  meal.id === item.mealId &&
+                  meal.restaurantId === item.restaurantId,
+              );
+              return (
+                <div className={" border-b p-2 py-6 px-6"}>
+                  <div key={item.mealId} className={"flex justify-between "}>
+                    <Typography className={"text-xl"}>
+                      {mealItem?.name}
+                    </Typography>
+                    <div className={"flex gap-2"}>
+                      <InputNumber
+                        min={1}
+                        className={"w-14"}
+                        defaultValue={item.quantity}
+                      />
+                      <Typography className={"text-xl"}></Typography>
+                      <Button type={"default"} icon={<DeleteFilled />} />
+                    </div>
+                  </div>
+                  <Typography className={"text-xl"}>
+                    ${item.totalPrice}
+                  </Typography>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={"text-center py-6"}>
+          <Button
+            className={"w-3/4 "}
+            size={"large"}
+            icon={<ShoppingCartOutlined />}
+            type={"primary"}
+          >
+            Checkout
+          </Button>
+        </div>
       </div>
-      <button
-        className={"py-2 px-4 bg-red-500 text-white rounded"}
-        type={"button"}
-        onClick={handleClose}
-      >
-        Close
-      </button>
     </Modal>
   );
 }
